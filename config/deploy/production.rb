1da1004caaebe6de6f11fc499c0877cefd791b4f
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-05-22 13:28:57 +0000

    init unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
 server '54.168.7.2', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/c/Users/tomoh/.ssh/id_rsa'
-#/home/vagrant/workspace/manyo_work
-#/c/Users/tomoh/.ssh/id_rsa
+set :ssh_options, keys: '~/.ssh/id_rsa'
+#/home/vagrant/workspace/manyo_work\

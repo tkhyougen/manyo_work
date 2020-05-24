c023d6e04424c52b71d7fa09d9cff36c65b07976
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-05-24 04:37:07 +0000

    aws

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery.js
+//  require jquery/dist/jquery.js
+//= require jquery
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
 
